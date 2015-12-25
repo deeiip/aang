dfa6147e84ca69c7e6e5fd501d618968ec1999a1
deeiip <ping@dipanjan.me>
2015-12-25 20:50:47 +0530

    added news model v2

--- aang.py
+++ aang.py
@@ -11,6 +11,22 @@ db = SQLAlchemy(app)
 # app = Flask(__name__)
 
 
+class NewsModel(db.Model):
+    url = db.Column(db.String(200), primary_key=True)
+    about = db.Column(db.String(50))
+    entities = db.Column(db.Text)
+    concepts = db.Column(db.Text)
+    sentiment = db.Column(db.Text)
+    author = db.Column(db.String(150))
+    title = db.Column(db.String(400))
+
+    def __init__(self, url):
+        self.url = url
+
+    def __repr__(self):
+        return '<Url %r>' % self.url
+
+
 @app.route('/')
 @app.route('/Index')
 def hello_world():
--- news_data_model.py
+++ news_data_model.py
@@ -1,19 +1,2 @@
-from aang import db
-
-
-class NewsModel(db.Model):
-    url = db.Column(db.String(200), primary_key=True)
-    about = db.Column(db.String(50))
-    entities = db.Column(db.Text)
-    concepts = db.Column(db.Text)
-    sentiment = db.Column(db.Text)
-    author = db.Column(db.String(150))
-    title = db.Column(db.String(400))
-
-    def __init__(self, url):
-        self.url = url
-
-    def __repr__(self):
-        return '<Url %r>' % self.url
 
 

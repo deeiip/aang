22afced3e08b3810985eec5d5a92e09efd7a77f1
deeiip <ping@dipanjan.me>
2015-12-25 19:44:17 +0530

    create db

--- aang.py
+++ aang.py
@@ -1,5 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jlanaajgscrrlo:wXwt2_X1sKZwZ91Y8tFRbC" \
+                                        "BfrL@ec2-107-22-184-127.compute-1.amazonaws.com:5432/da1cenu2lbulpo"
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
 

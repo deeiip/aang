ecc713e1281cbf1aa735be0cd8aac22e95b7a559
deeiip <ping@dipanjan.me>
2015-12-25 20:48:01 +0530

    added news model

--- aang.py
+++ aang.py
@@ -1,21 +1,26 @@
 from flask import Flask
 from flask import render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+from news_data_model import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jlanaajgscrrlo:wXwt2_X1sKZwZ91Y8tFRbC" \
                                         "BfrL@ec2-107-22-184-127.compute-1.amazonaws.com:5432/da1cenu2lbulpo"
 db = SQLAlchemy(app)
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/Index')
 def hello_world():
+    news = NewsModel('http://test.com/gweg')
+    db.session.add(news)
+    db.session.commit()
     return render_template('index.html')
-    #return html_minify(rendered_html)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+

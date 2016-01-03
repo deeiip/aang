1649b7cfc47156e6f9eaa94e473879abb61001de
deeiip <ping@dipanjan.me>
2016-01-03 20:28:55 +0530

    corrected navigaion

---
+++ static/dist/js/router.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dipanjan on 3/1/16.
+ */
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+var targetLink = '/keywords?subject='+getParameterByName('subject');
+$('#entity-link').attr("href", targetLink);
+var senti_link = '/?subject='+getParameterByName('subject');
+$('#sentiment-link').attr('href', senti_link);
